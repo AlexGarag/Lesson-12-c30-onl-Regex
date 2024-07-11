6a98e575e4f823f7e47bc6a7d7463a405cf9b44b
alex garag <alex.garag@bk.ru>
2024-07-11 11:02:12 +0300

    самое начало работы

---
+++ src/by/tmc/c30/lesson12regex/EmailAnalyzer.java
@@ -0,0 +1,8 @@
+package by.tmc.c30.lesson12regex;
+
+public class EmailAnalyzer {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/by/tmc/c30/lesson12regex/PasswordAnalyzer.java
@@ -0,0 +1,7 @@
+package by.tmc.c30.lesson12regex;
+
+public class PasswordAnalyzer {
+    public static void main(String[] args) {
+
+    }
+}
