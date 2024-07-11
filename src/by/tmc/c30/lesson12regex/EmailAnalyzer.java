5277913522d31818a66a726760ac09140f64ed27
alex garag <alex.garag@bk.ru>
2024-07-11 11:30:57 +0300

    валидация email взлетела

--- src/by/tmc/c30/lesson12regex/EmailAnalyzer.java
+++ src/by/tmc/c30/lesson12regex/EmailAnalyzer.java
@@ -1,8 +1,19 @@
 package by.tmc.c30.lesson12regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailAnalyzer {
     public static void main(String[] args) {
-
+        String email = "test@tmc.c30.com";
+        String regexPattern = "^(.+)@(\\S+)$";
+        Pattern emailPattern = Pattern.compile(regexPattern);
+        Matcher emailMatcher = emailPattern.matcher(email);
+        System.out.println();
+        if (emailMatcher.find()) {
+            System.out.println(email + " is a valid email address");
+        } else {
+            System.out.println(email + " is a invalid email!");
+        }
     }
-
 }
