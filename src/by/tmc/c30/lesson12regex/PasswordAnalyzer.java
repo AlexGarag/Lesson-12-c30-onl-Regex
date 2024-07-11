600f8407d4356a4b7be544c0a0c6d9672ab19bc2
alex garag <alex.garag@bk.ru>
2024-07-11 11:23:30 +0300

    валидация пароля взлетела

--- src/by/tmc/c30/lesson12regex/PasswordAnalyzer.java
+++ src/by/tmc/c30/lesson12regex/PasswordAnalyzer.java
@@ -1,7 +1,19 @@
 package by.tmc.c30.lesson12regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PasswordAnalyzer {
     public static void main(String[] args) {
-
+        String password = "12@@fghA#";
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        System.out.println();
+        if (matcher.find()) {
+            System.out.println(password.substring(matcher.start(), matcher.end()) + " - valid password");
+        } else {
+            System.out.println("Invalid password!");
+        }
     }
 }
